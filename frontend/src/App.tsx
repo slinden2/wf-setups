d784ed1542fd54dc229dfe0f68a864309f5aea21
Samu Linden <samu.linden@gmail.com>
2020-09-13 16:25:12 +0200

    Removes a useless component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Auth from "./components/user/Auth";
 import ProtectedRoute from "./components/ProtectedRoute";
 import HomePage from "./components/HomePage";
-import AddSetupPage from "./components/AddSetupPage";
 
 const App = () => {
   return (
@@ -12,9 +11,6 @@ const App = () => {
       <Switch>
         <ProtectedRoute exact path="/">
           <HomePage />
-        </ProtectedRoute>
-        <ProtectedRoute path="/add-setup">
-          <AddSetupPage />
         </ProtectedRoute>
         <Route path="/auth">
           <Auth />
