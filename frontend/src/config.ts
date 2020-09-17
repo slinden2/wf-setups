665bb55b83dafcafaccd2e5cb350fb8d7a85f22a
Samu Linden <samu.linden@gmail.com>
2020-09-17 15:23:23 +0200

    Fixes frontend env files

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -5,21 +5,11 @@ const env = {
   isTest: process.env.NODE_ENV === "test",
 };
 
-if (env.isProd) {
-  if (!process.env.REACT_APP_DISCORD_AUTH_URL_PROD) {
-    throw new Error("REACT_APP_DISCORD_AUTH_URL_PROD missing from .env");
-  }
-}
-
-if (env.isDev || env.isTest) {
-  if (!process.env.REACT_APP_DISCORD_AUTH_URL_DEV) {
-    throw new Error("REACT_APP_DISCORD_AUTH_URL_DEV missing from .env");
-  }
+if (!process.env.REACT_APP_DISCORD_AUTH_URL) {
+  throw new Error("REACT_APP_DISCORD_AUTH_URL missing from .env");
 }
 
 export default {
   env,
-  discordAuthUrl: env.isProd
-    ? process.env.REACT_APP_DISCORD_AUTH_URL_PROD!
-    : process.env.REACT_APP_DISCORD_AUTH_URL_DEV!,
+  discordAuthUrl: process.env.REACT_APP_DISCORD_AUTH_URL_PROD!,
 };
