4b47f84a0c4d7726e5ab23b5a2ea21a226cb8490
Samu Linden <samu.linden@gmail.com>
2020-09-19 19:55:10 +0200

    Creates a basic setup page

--- frontend/src/components/SetupPage.tsx
+++ frontend/src/components/SetupPage.tsx
@@ -1,12 +1,49 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSetupContext } from "../context/SetupContext";
+import { StatType } from "../types/StatType";
+
+const statArray: Array<StatType> = [
+  "power",
+  "suspension",
+  "gear",
+  "differential",
+  "brake",
+];
 
 export const SetupPage = () => {
   const { id } = useParams<{ id: string }>();
   const { getSetup } = useSetupContext()!;
   const curSetup = getSetup(id);
-  console.log(curSetup);
 
-  return <div>{id}</div>;
+  if (!curSetup) {
+    return null;
+  }
+
+  return (
+    <section>
+      <table>
+        <tbody>
+          <tr>
+            <th>Track</th>
+            <td>{curSetup.track.name}</td>
+          </tr>
+          <tr>
+            <th>Vehicle</th>
+            <td>{curSetup.track.origin}</td>
+          </tr>
+          <tr>
+            <th>Vehicle</th>
+            <td>{curSetup.vehicle.name}</td>
+          </tr>
+          {statArray.map((stat) => (
+            <tr key={stat}>
+              <th>{stat.charAt(0).toUpperCase() + stat.slice(1)}</th>
+              <td>{curSetup[stat]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
 };
