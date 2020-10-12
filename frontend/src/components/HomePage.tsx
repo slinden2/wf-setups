0328c76cbd441e925aaa44e235b9f43703f30a67
Samu Linden <samu.linden@gmail.com>
2020-10-12 20:56:54 +0200

    Adds table to the page

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import { useSetupContext } from "../context/SetupContext";
+import Loader from "../styles/elements/Loader";
 import { Title } from "../styles/elements/Title";
+import StyledDataTable from "./table/StyledDataTable";
+import { columns } from "./table/tableData";
 
 const HomePage: React.FC = () => {
   const { getSetupSuggestions } = useSetupContext();
 
   const setups = getSetupSuggestions();
-  console.log(setups);
+
+  if (setups.loading) {
+    return <Loader text="Loading" />;
+  }
+
+  if (!setups.data) {
+    return <div>Nothing to see here</div>;
+  }
+
+  const tableData = setups.data.map((setup) => ({
+    ...setup,
+    track: setup.track.name,
+    vehicle: setup.vehicle.name,
+  }));
 
   return (
     <div>
-      <Title>Setups</Title>
+      <Title>Setup Suggestions</Title>
+      <StyledDataTable
+        columns={columns}
+        data={tableData}
+        striped
+        noHeader
+        highlightOnHover
+        dense
+        responsive
+      />
     </div>
   );
 };
