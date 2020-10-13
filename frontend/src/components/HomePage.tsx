2a8169749327c575cabd4bd35b1bd1d81438997b
Samu <samu.linden@gmail.com>
2020-10-13 12:36:38 +0200

    Adds control for input field

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { useSetupContext } from "../context/SetupContext";
 import Loader from "../styles/elements/Loader";
 import { Title } from "../styles/elements/Title";
+import { StyledInput } from "./form/InputField";
 import StyledDataTable from "./table/StyledDataTable";
 import { columns } from "./table/tableData";
 
 const HomePage: React.FC = () => {
+  const [searchStr, setSearchStr] = React.useState<string>("");
   const { getSetupSuggestions } = useSetupContext();
+
+  console.log(searchStr);
 
   const setups = getSetupSuggestions();
 
@@ -18,6 +22,10 @@ const HomePage: React.FC = () => {
     return <div>Nothing to see here</div>;
   }
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchStr(e.currentTarget.value);
+  };
+
   const tableData = setups.data.map((setup) => ({
     ...setup,
     track: setup.track.name,
@@ -27,6 +35,14 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <Title>Setup Suggestions</Title>
+      <StyledInput
+        name="search"
+        placeholder="Search setups..."
+        maxWidth="400px"
+        marginBottom="1rem"
+        onChange={(e) => handleSearch(e)}
+        value={searchStr}
+      />
       <StyledDataTable
         columns={columns}
         data={tableData}
